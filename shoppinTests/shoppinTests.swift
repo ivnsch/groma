caa8f5a7e1949b49bc8ffeb61879a8e238fbeab8
ischuetz <ivanschuetz@gmail.com>
2014-12-06 13:58:22 +0100

    Initial Commit

---
+++ shoppinTests/shoppinTests.swift
@@ -0,0 +1,36 @@
+//
+//  shoppinTests.swift
+//  shoppinTests
+//
+//  Created by ischuetz on 06.12.14.
+//  Copyright (c) 2014 ivanschuetz. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class shoppinTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
