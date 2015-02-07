4ae38e392178cdf59fc67593f29739eecdebc9e1
ischuetz <ivanschuetz@gmail.com>
2015-02-07 11:20:02 +0100

    Added osx project, update cocoapods version

---
+++ shoppin_osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  shoppin_osx
+//
+//  Created by ischuetz on 07.02.15.
+//  Copyright (c) 2015 ivanschuetz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ shoppin_osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  shoppin_osx
+//
+//  Created by ischuetz on 07.02.15.
+//  Copyright (c) 2015 ivanschuetz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
