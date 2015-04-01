7fbec53132c02cb4bff187d9ca66602564d2dc14
ischuetz <ivanschuetz@gmail.com>
2015-04-01 14:14:38 +0200

    Add tableview extension, method to get absolute row index

---
+++ shoppin/UITableView.swift
@@ -0,0 +1,20 @@
+//
+//  UITableView.swift
+//  shoppin
+//
+//  Created by ischuetz on 01/04/15.
+//  Copyright (c) 2015 ivanschuetz. All rights reserved.
+//
+
+import UIKit
+
+extension UITableView {
+   
+    func absoluteRow(indexPath: NSIndexPath) -> Int {
+        var absRow = indexPath.row
+        for section in 0..<indexPath.section {
+            absRow += self.numberOfRowsInSection(section)
+        }
+        return absRow
+    }
+}
