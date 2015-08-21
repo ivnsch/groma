c3aa84fbd656c63bdc79d92274cc08ca56c5d2de
ischuetz <ivanschuetz@gmail.com>
2015-08-21 11:02:04 +0200

    Fix inventory item primary key

--- DBInventoryItem.swift
+++ DBInventoryItem.swift
@@ -11,14 +11,19 @@ import RealmSwift
 
 class DBInventoryItem: DBSyncable {
     
-    dynamic var id: Int = 0
     dynamic var quantity: Int = 0
     dynamic var quantityDelta: Int = 0
     dynamic var product: DBProduct = DBProduct()
     dynamic var inventory: DBInventory = DBInventory()
     
+    dynamic lazy var compoundKey: String = self.compoundKeyValue()
+
+    private func compoundKeyValue() -> String {
+        return "\(product.uuid)-\(inventory.uuid)"
+    }
+
     override static func primaryKey() -> String? {
-        return "id"
+        return "compoundKey"
     }
 }
 
