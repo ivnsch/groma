5bc51189f9724f93ba81afb530e19c6a92ed6256
Ivan Schuetz <ivanschuetz@gmail.com>
2017-03-01 00:24:31 +0100

    Enable server for all countries

--- CountryHelper.swift
+++ CountryHelper.swift
@@ -20,12 +20,14 @@ class CountryHelper {
     
     static func isInServerSupportedCountry() -> Bool {
         
-        let countryCodeMaybe = currentDeviceCountry()?.lowercased()
-        let isSupported = countryCodeMaybe.map{serverSupportedCountries.contains($0)} ?? false
+        return true // we will release only to countries where the server is also supported
         
-        QL1("Is server supported country: \(countryCodeMaybe), isSupported: \(isSupported)")
-        
-        return isSupported
-//        return true
+//        let countryCodeMaybe = currentDeviceCountry()?.lowercased()
+//        let isSupported = countryCodeMaybe.map{serverSupportedCountries.contains($0)} ?? false
+//        
+//        QL1("Is server supported country: \(countryCodeMaybe), isSupported: \(isSupported)")
+//        
+//        return isSupported
+////        return true
     }
 }
