bacdaf8e69d7794c5bbd40db7a2763dc2bc0984a
Ivan Schuetz <ivanschuetz@gmail.com>
2017-03-14 21:20:22 +0100

    Disable swipe to delete in list item cells
    The reason is that it looks weird here as we have reorder control and it moves only the (custom) content view which the delete button between this view and the reorder control instead of at the right side. It's particularly weird as it's still possible to delete using default iOS animation which shows it correctly at the right side. In inventory items we can do swipe to delete because it doesn't have reorder control.

--- shoppin/ListItemCellNew.swift
+++ shoppin/ListItemCellNew.swift
@@ -86,7 +86,7 @@ class ListItemCellNew: SwipeableCell, SwipeToIncrementHelperDelegate, QuantityVi
         didSet {
             if let tableViewListItem = tableViewListItem {
                 
-                swipeToDeleteHelper?.setOpen(false, animated: false) // recycling
+//                swipeToDeleteHelper?.setOpen(false, animated: false) // recycling
 
                 let listItem = tableViewListItem
                 
@@ -251,8 +251,8 @@ class ListItemCellNew: SwipeableCell, SwipeToIncrementHelperDelegate, QuantityVi
         //        let tapRecognizer = UITapGestureRecognizer(target: self, action: "onTapPlusMinusContainer:")
         //        minusButton.addGestureRecognizer(tapRecognizer)
         
-        swipeToDeleteHelper = SwipeToDeleteHelper(parentView: self, button: myContentView, leftLayoutConstraint: contentViewLeftConstraint, rightLayoutConstraint: contentViewRightConstraint, cancelTouches: false)
-        swipeToDeleteHelper?.delegate = self
+//        swipeToDeleteHelper = SwipeToDeleteHelper(parentView: self, button: myContentView, leftLayoutConstraint: contentViewLeftConstraint, rightLayoutConstraint: contentViewRightConstraint, cancelTouches: false)
+//        swipeToDeleteHelper?.delegate = self
         
         swipeToIncrementHelper = SwipeToIncrementHelper(view: myContentView)
         swipeToIncrementHelper?.delegate = self
