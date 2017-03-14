f84a26ca6b606fe859905ea20c14e48bed766691
Ivan Schuetz <ivanschuetz@gmail.com>
2017-03-14 21:39:48 +0100

    Fix sometimes quantity view truncated during swipe

--- TextFieldMore.swift
+++ TextFieldMore.swift
@@ -24,9 +24,16 @@ class TextFieldMore: UITextField {
     
     // Invalidate intrinsic size while typing doesn't work (field doesn't change its width) so we need this. See also http://stackoverflow.com/questions/18236661/resize-a-uitextfield-while-typing-by-using-autolayout
     override var intrinsicContentSize: CGSize {
+        
         if calculateIntrinsicSizeManually {
+            
             if let font = font {
-                return text?.size(font) ?? CGSize.zero
+                
+                return text.map{
+                    let textSize = $0.size(font)
+                    return CGSize(width: textSize.width + 2, height: textSize.height) // + 2 "magic number" for some reason sometimes the label gets truncated (e.g. when we change the quantity with swiping - at some numbers. With + 2 it doesn't happen
+                } ?? CGSize.zero
+                
             } else {
                 return CGSize.zero
             }
