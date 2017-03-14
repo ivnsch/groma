a5e0dc569f21cc517663ba7e6504438869795e57
Ivan Schuetz <ivanschuetz@gmail.com>
2017-03-14 22:22:16 +0100

    Add swipe to increment to ingredient quantity view

--- SelectIngredientQuantityController.swift
+++ SelectIngredientQuantityController.swift
@@ -8,15 +8,40 @@
 
 import UIKit
 
-class SelectIngredientQuantityController: UIViewController {
+class SelectIngredientQuantityController: UIViewController, SwipeToIncrementHelperDelegate {
 
     @IBOutlet weak var quantityView: QuantityView!
-
+    @IBOutlet weak var quantityViewContainer: UIView!
+    
     var onUIReady: (() -> Void)?
+    
+    fileprivate var swipeToIncrementHelper: SwipeToIncrementHelper?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        swipeToIncrementHelper = SwipeToIncrementHelper(view: quantityViewContainer)
+        swipeToIncrementHelper?.delegate = self
+
         onUIReady?()
     }
+    
+    // MARK: - SwipeToIncrementHelperDelegate
+    
+    func currentQuantity() -> Float {
+        return quantityView.quantity
+    }
+    
+    func onQuantityUpdated(_ quantity: Float) {
+        quantityView.quantity = quantity
+    }
+    
+    func onFinishSwipe() {
+    }
+    
+    var swipeToIncrementEnabled: Bool {
+        return true
+    }
+    
 }
