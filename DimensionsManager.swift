2fc509202138e3f77715f5e7c79152d5c35fc1b7
Ivan Schuetz <ivanschuetz@Ivans-Mac-mini.local>
2017-03-27 12:55:47 +0200

    Use default cell height for ingredients & items

--- DimensionsManager.swift
+++ DimensionsManager.swift
@@ -204,12 +204,13 @@ class DimensionsManager {
     }
     
     static var ingredientsCellHeight: CGFloat {
-        switch heightDimension {
-        case .verySmall: return 50
-        case .small: return 50
-        case .middle: return 60
-        case .large: return 60
-        }
+        return defaultCellHeight
+//        switch heightDimension {
+//        case .verySmall: return 50
+//        case .small: return 50
+//        case .middle: return 60
+//        case .large: return 60
+//        }
     }
     
     static var ingredientsUnitCellHeight: CGFloat {
